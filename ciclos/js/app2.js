20a1a43d8223085e8eafc5d77ffe2c9d0bfd4f2e
emikourtis <126197808+emikourtis@users.noreply.github.com>
2023-03-16 08:50:02 -0300

    ciclos terminado

---
+++ ciclos/js/app2.js
@@ -0,0 +1,14 @@
+alert('VAS A INGRESAR NUMEROS Y TE DIRE CUANTOS NUMEROS PARES E IMPARES INGRESASTE');
+alert('CUANDO NO DESEAS INGRESAR MAS NUMEROS ESCRIBE SALIR')
+
+
+do{
+    numero = parseInt(prompt('INGRESE UN NUMERO: '));
+    if (numero % 2 == 0) {
+        console.log(numero+' :NUMERO PAR');
+    } else {
+        console.log(numero+' :NUMERO IMPAR');
+    }
+}while (parseInt(numero))
+    
+
