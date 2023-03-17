da711d836410a497913edf59ebf812304815a491
emikourtis <126197808+emikourtis@users.noreply.github.com>
2023-03-17 08:39:46 -0300

    añadi comentario

--- ciclos/js/app2.js
+++ ciclos/js/app2.js
@@ -1,3 +1,9 @@
+/*
+Actividad 02:
+Crea un algoritmo que le pida al usuario un número y luego utiliza while para evaluar
+cuántos números son pares y cuantos impares, hasta que el usuario ingrese "salir".
+*/
+
 alert('VAS A INGRESAR NUMEROS Y TE DIRE CUANTOS NUMEROS PARES E IMPARES INGRESASTE');
 alert('CUANDO NO DESEAS INGRESAR MAS NUMEROS ESCRIBE SALIR')
 
@@ -9,6 +15,6 @@ do{
     } else {
         console.log(numero+' :NUMERO IMPAR');
     }
-}while (parseInt(numero))
+}while (parseInt(numero)) 
     
 
